d5cbb98161bcebb31d7b974d5305b399c1e6906c
Shang Zhou Xia <shangxzx@gmail.com>
2017-09-23 15:30:52 -0400

    Updates to personal project using alias

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ faceRec.post('/', function(req, res) {
             'content-type': 'application/json',
             'ocp-apim-subscription-key': '60c7f169fbe24c12b5990a8916315bd7'
         },
-        body: { url: req.body },
+        body: { "url": req.body },
         json: true
     };
 
